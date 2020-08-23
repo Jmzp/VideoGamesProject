9f5551f850d775fa623d0193f60af9914b1aac7d
andreact <andrea.c.t.1996@gmail.com>
2020-08-23 00:05:22 -0500

    Include life bar and life script

---
+++ Assets/Scripts/life.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class life : MonoBehaviour
+{
+    float health;
+    public GameObject LifeBarImage;
+    public GameObject GameOverImage;
+    void Start()
+    {
+        this.health = 1f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.health -= 0.205f;
+        this.LifeBarImage.GetComponent<Image>().fillAmount = this.health;
+
+        if (this.health <= 0)
+            this.GameOverImage.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
