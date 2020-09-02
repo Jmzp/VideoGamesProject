dedfd3b23955de45f16baa00e579ce2d95d1adb9
Jorge Zapata <cvem8165@gmail.com>
2020-09-02 16:00:55 -0500

    Add enemies dead

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,54 +5,66 @@ public class Enemy : MonoBehaviour
 {
     private const string MOVE = "Move";
     private const string ATTACK = "Attack";
+    private const string STATE_DIE = "Die";
     public GameObject objective; // Player to be followed by the enemy
     private Vector3 initialPosition;
     private NavMeshAgent agent;
-    bool haObstacle;
+    private bool haObstacle;
+    private Animator animator;
+    private bool isDead;
 
     // Start is called before the first frame update
     void Start()
     {
         this.initialPosition = this.transform.position;
         this.agent = GetComponent<NavMeshAgent>();
+        this.animator = GetComponent<Animator>();
+        this.isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        NavMeshHit navMeshHit;
-        this.haObstacle = NavMesh.Raycast(this.transform.position, this.objective.transform.position, out navMeshHit, NavMesh.AllAreas);
-        Debug.DrawLine(this.transform.position, this.objective.transform.position, this.haObstacle ? Color.red : Color.green);
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(STATE_DIE)) {
+            this.isDead = true;
+            Destroy(this.gameObject, 1.2f);
+        }
+        else if (!isDead)
+        {
+            NavMeshHit navMeshHit;
+            this.haObstacle = NavMesh.Raycast(this.transform.position, this.objective.transform.position, out navMeshHit, NavMesh.AllAreas);
+            Debug.DrawLine(this.transform.position, this.objective.transform.position, this.haObstacle ? Color.red : Color.green);
 
-        if (!this.haObstacle)
-        {
-            float distance = Vector3.Distance(this.transform.position, this.objective.transform.position);
-            transform.LookAt(this.objective.transform);
-            this.agent.isStopped = false;
-            MoveTo(this.objective.transform.position);
-            this.AttackAnimation(0);
-            this.MoveAnimation(1);
-            if (distance <= 1.7f)
+            if (!this.haObstacle)
             {
-                this.agent.isStopped = true;
-                this.AttackAnimation(1);
-                //GetComponent<AudioSource>().Play();
+                float distance = Vector3.Distance(this.transform.position, this.objective.transform.position);
+                transform.LookAt(this.objective.transform);
+                this.agent.isStopped = false;
+                MoveTo(this.objective.transform.position);
+                this.AttackAnimation(0);
+                this.MoveAnimation(1);
+                if (distance <= 2f)
+                {
+                    this.agent.isStopped = true;
+                    this.AttackAnimation(1);
+                    //GetComponent<AudioSource>().Play();
+                }
             }
-        }
-        else
-        {
-            MoveTo(this.initialPosition);
-        }
-        if (this.haObstacle && this.agent.remainingDistance < 1)
-        {
-            this.MoveAnimation(0);
-            this.AttackAnimation(0);
+            else
+            {
+                MoveTo(this.initialPosition);
+            }
+            if (this.haObstacle && this.agent.remainingDistance < 1)
+            {
+                this.MoveAnimation(0);
+                this.AttackAnimation(0);
+            }
         }
     }
 
     void MoveAnimation(int state)
     {
-        GetComponent<Animator>().SetInteger(MOVE, state);
+        animator.SetInteger(MOVE, state);
     }
 
     void AttackAnimation(int state)
--- Assets/Scripts/LifeE.cs
+++ Assets/Scripts/LifeE.cs
@@ -1,17 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
+﻿using UnityEngine;
 
 public class LifeE : MonoBehaviour
 {
-    //public GameObject GameOverImage;
+    private const string DEAD = "Dead";
     public int MaxHits;
+
     // Start is called before the first frame update
     void Start()
     {
-        MaxHits = this.MaxHits;
+
     }
 
     // Update is called once per frame
@@ -22,17 +19,19 @@ public class LifeE : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "PolyartSword" && this.MaxHits <= 5)
+        if (other.name == "PolyartSword" && this.MaxHits >= 0)
         {
             this.MaxHits -= 1;
-            print("prueba");
         }
         if (this.MaxHits == 0)
         {
-            Destroy(this.gameObject);
-            print("Muerto");
+            DeadAnimation();
         }
-            //this.GameOverImage.SetActive(true);
-           //Destroy(other.transform.gameObject);
     }
+
+    private void DeadAnimation()
+    {
+        GetComponent<Animator>().SetInteger(DEAD, 1);
+    }
+
 }
